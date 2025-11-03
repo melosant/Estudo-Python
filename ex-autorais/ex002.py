d027c12f30299c5e557cfad4637422b6e3329d35
Nathã Melo <nathamelo2007@gmail.com>
2025-11-03 17:59:23 -0300

    Remoção pasta ex-autorais da raiz

--- ex-autorais/ex002.py
+++
@@ -1,9 +0,0 @@
-# Programa 2 - Par ou Ímpar
-
-num = int(input("Digite um número : "))
-
-# estrutura condicional para verificação
-if num % 2 == 0:
-    print(f"{num} é PAR!")
-else:
-    print(f"{num} é ÍMPAR!")
--- ex-autorais/ex005.py
+++
@@ -1,6 +0,0 @@
-# Programa 5 - Mensagem de Boas Vindas!
-
-nome = input("Digite seu nome : ")
-idade = int(input(("Por gentileza, informe a sua idade : ")))
-
-print(f"Olá {nome}! Você tem {idade} anos.")
