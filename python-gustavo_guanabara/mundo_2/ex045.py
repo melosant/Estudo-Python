7aa32581147294b2157caa70eabaa4dc94b5176f
Nathã Melo <nathamelo2007@gmail.com>
2025-11-19 20:02:00 -0300

    ex045

---
+++ python-gustavo_guanabara/mundo_2/ex045.py
@@ -0,0 +1,50 @@
+import random
+import time
+
+dic = {1 : 'PEDRA', 2 : 'PAPEL', 3 : 'TESOURA'}
+
+print('=-=-=-=-=-=-=-=-=-=-=-=')
+print('PEDRA, PAPEL OU TESOURA')
+print('=-=-=-=-=-=-=-=-=-=-=-=')
+print('''\nSUAS OPÇÕES :
+[1] PEDRA
+[2] PAPEL
+[3] TESOURA
+      SELECIONE (1-3) : ''')
+
+maquina = random.randint(1,3)
+jogador = int(input())
+
+print('\nCOMPUTADOR JOGANDO')
+print('.')
+time.sleep(.5)
+print('.')
+time.sleep(.5)
+print('.')
+time.sleep(1)
+
+if jogador == 1:
+    if maquina == 1:
+        print(f'{dic[jogador]} x {dic[maquina]} = Empate!!')
+    elif maquina == 2:
+        print(f'{dic[jogador]} x {dic[maquina]} = Computador Venceu!!')
+    elif maquina == 3:
+        print(f'{dic[jogador]} x {dic[maquina]} = O Jogador Venceu!!')
+
+elif jogador == 2:
+    if maquina == 1:
+        print(f'{dic[jogador]} x {dic[maquina]} = O Jogador Venceu!!')
+    elif maquina == 2:
+        print(f'{dic[jogador]} x {dic[maquina]} = Empate!!')
+    elif maquina == 3:
+        print(f'{dic[jogador]} x {dic[maquina]} = O Computador Venceu!!')
+
+elif jogador == 3:
+    if maquina == 1:
+        print(f'{dic[jogador]} x {dic[maquina]} = O Computador Venceu!!')
+    elif maquina == 2:
+        print(f'{dic[jogador]} x {dic[maquina]} = O Jogador Venceu!!')
+    elif maquina == 3:
+        print(f'{dic[jogador]} x {dic[maquina]} = Empate!!')
+else:
+    print('Jogada Inválida.')
