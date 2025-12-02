513430959661c4e2dbe3bc92866d95441cf10519
Nathã Melo <nathamelo2007@gmail.com>
2025-12-02 13:12:49 -0300

    Try Except ex06

--- py3-udemy/exercícios/section3/ex06.py
+++ py3-udemy/exercícios/section3/ex06.py
@@ -1,12 +1,15 @@
 # Saudação de acordo com o horário
 # Aula 55
 
-hora = int(input('Digite a hora atual (h): '))
-if hora >= 0 and hora < 12:
-    print('Bom dia!')
-elif hora >= 12 and hora < 18:
-    print('Boa Tarde!')
-elif hora >= 18 and hora <= 23:
-    print('Boa Noite!')
-else:
-    print('Hora não válida.')
+try:
+    hora = int(input('Digite a hora atual (h): '))
+    if hora >= 0 and hora < 12:
+        print('Bom dia!')
+    elif hora >= 12 and hora < 18:
+        print('Boa Tarde!')
+    elif hora >= 18 and hora <= 23:
+        print('Boa Noite!')
+    else:
+        print('Hora não válida.')
+except:
+    print('Hora digitada não válida.')
