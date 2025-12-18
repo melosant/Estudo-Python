c03c4982dcaa78ba3302b0b8a4b5539b497df6b3
Nathã <nathamelo2007@gmail.com>
2025-12-18 12:58:36 -0300

    style: comentario enunciado ex dia 2

--- pandas-2025/dia02/exercicios.py
+++ pandas-2025/dia02/exercicios.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
+'''
+Leia o arquivo transacoes.csv com a formatação correta;
+Adicione uma coluna com valores 1;
+Salve o dataframe com nome: transacoes_1.csv
+'''
+
 transacoes = pd.read_csv('pandas-2025/data/transacoes.csv', sep=';')
 transacoes['valores_1'] = 1
 print(transacoes)
